e76ee73d3bc28977a00ef6782c98c703b78b3cbe
Arsh Kumar <ararsh50@gmail.com>
2024-07-06 13:17:44 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,13 +5,20 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+
+
+/*
+const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
 });
-
+*/
 // Example of using the pool to query the database
 pool.query('SELECT NOW()', (err, res) => {
     if (err) {
